dc3f2fe134f485f4ee61d55e2393eae8f41b1584
sebuhiheziyev <sebuhiheziyev39@gmail.com>
2023-10-08 16:22:18 +0400

    project add github

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("myDIV").addEventListener("click", myFunction);
+
+
+function myFunction() {
+    var menu = document.getElementById("myMENU");
+    menu.classList.toggle("mystyle");
+}
