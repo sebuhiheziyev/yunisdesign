5a6cfd26968c015889d509026432ddb4c12666d3
sebuhiheziyev <sebuhiheziyev39@gmail.com>
2023-10-26 01:05:35 +0400

    worked

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-
-
 document.getElementById("myDIV").addEventListener("click", myFunction);
-
 
 function myFunction() {
     var menu = document.getElementById("myMENU");
     menu.classList.toggle("mystyle");
+}
+
+
+document.getElementById("contactClik").addEventListener("click", scrollFunc);
+document.getElementById("contactClikMobile").addEventListener("click", scrollFunc);
+
+function scrollFuncForMobile() {
+    const element = document.getElementById("contact");
+    element.scrollIntoView();
+}
+
+function scrollFunc() {
+    const element = document.getElementById("contact");
+    element.scrollIntoView();
 }
